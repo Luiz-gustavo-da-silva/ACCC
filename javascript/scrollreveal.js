036b4e61457ec5098328ec3ec9a3ac130ade9e59
Luiz Gustavo da Silva <38591825+Luiz4us8av6@users.noreply.github.com>
2022-08-27 01:53:15 -0300

    commit correção teste de efeito

--- javascript/scrollreveal.js
+++ javascript/scrollreveal.js
@@ -1,13 +1,5 @@
-window.sr = ScrollReveal({reset: true});
+window.sr = ScrollReveal({ reset: true });
 
-sr.reveal('.efeito_scroll',{
-    rotate:{x:0,y:80,z:0}, 
-    duration:1000
-
-});
-
-sr.reveal('.efeito_scroll_textos',{
-    duration:1000
-
-});
+sr.reveal('.efeito_scroll', { duration:1000,  rotate:{x:0,y:80,z:0}});
+sr.reveal('.efeito_scroll_textos', { duration:1000 });
 
