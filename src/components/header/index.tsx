792835b8f15c9cf13d3a9c099538ee87284b9c12
Luiz Gustavo da Silva <38591825+Luiz4us8av6@users.noreply.github.com>
2023-07-21 22:08:23 -0300

    feat: adição de detalhes

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,30 +1,9 @@
-// import React, { useEffect, useState } from "react";
 import style from "./index.module.css";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 const Header = () => {
-
-  /*const [scrolled, setScrolled] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const isScrolled = window.scrollY > 0;
-      if (isScrolled !== scrolled) {
-        setScrolled(isScrolled);
-      }
-    };
-
-    document.addEventListener("scroll", handleScroll);
-
-    return () => {
-      document.removeEventListener("scroll", handleScroll);
-    };
-  }, [scrolled]);
-
-  const navbarClass = scrolled ? `${style.container} ${style.scrolled}` : style.container;*/
-
 
   return (
     <Navbar expand="lg" className={style.container}>
@@ -33,14 +12,14 @@ const Header = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse className={style.container__navbar}>
           <Nav>
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#sobre">Sobre nós</Nav.Link>
-            <Nav.Link href="#produtos">Produtos</Nav.Link>
-            <Nav.Link href="#casa">Casa de sementes</Nav.Link>
-            <Nav.Link href="#noticias">Notícias</Nav.Link>
-            <Nav.Link href="#galeria">Galeria</Nav.Link>
-            <Nav.Link href="#contatos">Contatos</Nav.Link>
-            <Nav.Link href="#parceiros">Parceiros</Nav.Link>
+            <Nav.Link href="#home" className={style.link}>Home</Nav.Link>
+            <Nav.Link href="#sobre" className={style.link}>Sobre nós</Nav.Link>
+            <Nav.Link href="#produtos" className={style.link}>Produtos</Nav.Link>
+            <Nav.Link href="#casa" className={style.link}>Casa de sementes</Nav.Link>
+            <Nav.Link href="#noticias" className={style.link}>Notícias</Nav.Link>
+            <Nav.Link href="#galeria" className={style.link}>Galeria</Nav.Link>
+            <Nav.Link href="#contatos" className={style.link}>Contatos</Nav.Link>
+            <Nav.Link href="#parceiros" className={style.link}>Parceiros</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
