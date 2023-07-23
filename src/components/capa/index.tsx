4ff11f6d96baf8f368f0006015eccf3c9aa10dbb
Luiz Gustavo da Silva <38591825+Luiz4us8av6@users.noreply.github.com>
2023-07-23 18:39:13 -0300

    feat: finalização da tela mob home e capa

--- src/components/capa/index.tsx
+++ src/components/capa/index.tsx
@@ -3,7 +3,20 @@ import style from "./index.module.css";
 const Capa = () => {
   return (
     <div className={style.container}>
-      <div className={style.modulo__capa}></div>
+      <div className={style.modulo__capa}>
+        <div className={style.capaEscura}>
+          <div>
+          <div>
+            <p>Colaboradores</p>
+            <span>+40</span>
+          </div>
+          <div>
+            <p>Associados</p>
+            <span>+30</span>
+          </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/galeria/index.tsx
+++ src/components/galeria/index.tsx
@@ -1,23 +1,95 @@
-import Button  from "../button";
+import Button from "../button";
 import style from "./index.module.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+// import Button from "../button";
+import { Navigation } from "swiper/modules";
 
 const Galeria = () => {
   return (
     <div className={style.container}>
-        <div className={style.container__header}>
-            <div className={style.container__tituloSessoes}><h2>Galeria. </h2><span> Veja alguns dos nossos registros.</span></div>
-            <div><Button nome={"Vizualizar todas"} tipo={1}/></div>
+      <div className={style.container__header}>
+        <div className={style.container__tituloSessoes}>
+          <h2>Galeria. </h2>
+          <span> Veja alguns dos nossos registros.</span>
         </div>
+        <div>
+          <Button nome={"Vizualizar todas"} tipo={1} />
+        </div>
+      </div>
 
-        <div className={style.container__grid}>
-            <div id={style.img1} className={style.celula}><img src="/assets/capa.png" alt="" /></div>
-            <div id={style.img2} className={style.celula}><img src="/assets/capa.png" alt="" /></div>
-            <div id={style.img3} className={style.celula}><img src="/assets/capa.png" alt="" /></div>
-            <div id={style.img4} className={style.celula}><img src="/assets/capa.png" alt="" /></div>
-            <div id={style.img5} className={style.celula}><img src="/assets/capa.png" alt="" /></div>
-            <div id={style.img6} className={style.celula}><img src="/assets/capa.png" alt="" /></div>
-            <div id={style.img7} className={style.celula}><img src="/assets/capa.png" alt="" /></div>
+      <div className={style.container__grid}>
+        <div id={style.img1} className={style.celula}>
+          <img src="/assets/capa.png" alt="" />
         </div>
+        <div id={style.img2} className={style.celula}>
+          <img src="/assets/capa.png" alt="" />
+        </div>
+        <div id={style.img3} className={style.celula}>
+          <img src="/assets/capa.png" alt="" />
+        </div>
+        <div id={style.img4} className={style.celula}>
+          <img src="/assets/capa.png" alt="" />
+        </div>
+        <div id={style.img5} className={style.celula}>
+          <img src="/assets/capa.png" alt="" />
+        </div>
+        <div id={style.img6} className={style.celula}>
+          <img src="/assets/capa.png" alt="" />
+        </div>
+        <div id={style.img7} className={style.celula}>
+          <img src="/assets/capa.png" alt="" />
+        </div>
+      </div>
+
+      <div className={style.containerSlide}>
+        <Swiper
+          breakpoints={{
+            // Configurações para dispositivos de tela grande (desktop)
+            1024: {
+              slidesPerView: 3,
+              spaceBetween: 30,
+            },
+            // Configurações para dispositivos de tela média e pequena
+            0: {
+              slidesPerView: 1,
+              spaceBetween: 10,
+            },
+          }}
+          centeredSlides={false}
+          navigation={true}
+          modules={[Navigation]}
+          className="mySwiper"
+        >
+          <SwiperSlide className={style.swiper__slide}>
+            <div>
+              <img src="/assets/capa.png" alt="" />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className={style.swiper__slide}>
+            <div>
+              <img src="/assets/capa.png" alt="" />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className={style.swiper__slide}>
+            <div>
+              <img src="/assets/capa.png" alt="" />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className={style.swiper__slide}>
+            <div>
+              <img src="/assets/capa.png" alt="" />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className={style.swiper__slide}>
+            <div>
+              <img src="/assets/capa.png" alt="" />
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      </div>
     </div>
   );
 };
