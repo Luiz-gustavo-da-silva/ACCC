d6532c703f09d3e4f10bc8c8972c74d8ef6253fa
Luiz Gustavo da Silva <38591825+Luiz4us8av6@users.noreply.github.com>
2023-07-30 21:56:22 -0300

    feat: finalização do modulo casa de semente

--- src/components/casaSementes/index.tsx
+++ src/components/casaSementes/index.tsx
@@ -1,5 +1,16 @@
 import style from "./index.module.css";
 import Container from "react-bootstrap/Container";
+// import type { CollapseProps } from "antd";
+import { Collapse } from "antd";
+const { Panel } = Collapse;
+
+const header = (
+  <p style={{ paddingLeft: 24 }}>
+    A dog is a type of domesticated animal. Known for its loyalty and
+    faithfulness, it can be found as a welcome guest in many households across
+    the world.
+  </p>
+);
 
 const CasaSementes = () => {
   return (
@@ -8,44 +19,133 @@ const CasaSementes = () => {
         <h2>Casa de sementes. </h2>
         <span>Conheça nossa iniciativa de auto-suficiência.</span>
       </div>
-      <div className={style.container__div}>
-        <div className={style.container__grid}>
-          <div id={style.modulo1} className={style.celula}>
-            <div>
-              <p>Tipos de sementes</p>
-              <h1>
-                <span className={style.mais}>+</span>50
-              </h1>
-              <p>
-                Em maior quantidade:
-                <span>Feijão</span>
-                <span>Milho</span>
-                <span>Fava</span>
-              </p>
-            </div>
-          </div>
 
-          <div id={style.modulo2} className={style.celula}>
-            <p>
-              Texto aleatório aqui Texto aleatório aqui Texto Texto aleatório
-              aqui Texto aleatório aleatório aqui Texto aleatório aqui aleatório
-              aqui Texto Texto aleatório aqui Texto aleatório aqui Texto Texto
-              aleatório aqui Texto aleatório aleatório aqui Texto.
-            </p>
-            <p>
-              Texto aleatório aqui Texto aleatório aqui Texto Texto aleatório
-              aqui Texto aleatório aleatório aqui Texto aleatório aqui aleatório
-              aqui Texto Texto aleatório aqui Texto aleatório aqui Texto Texto
-              aleatório aqui Texto aleatório aleatório aqui Texto.
-            </p>
-            <p>
-              Texto aleatório aqui Texto aleatório aqui Texto Texto aleatório
-              aqui Texto aleatório aleatório aqui Texto aleatório aqui aleatório
-              aqui Texto Texto aleatório aqui Texto aleatório aqui Texto Texto
-              aleatório aqui Texto aleatório aleatório aqui Texto.
-            </p>
-          </div>
-        </div>
+      <div className={style.container__collapse}>
+        <Collapse
+          bordered={false}
+          defaultActiveKey={["1"]}
+          className={style.collapse}
+        >
+          <Panel
+            header={
+              <div className={style.header}>
+                <div>
+                  <p >Feijão</p>
+                </div>
+                <div>
+                  <span>15</span> Espécies
+                </div>
+              </div>
+            }
+            key="1"
+          >
+            <div style={{ display: "flex" }}>
+              <div>
+                <span>14</span> espécies
+              </div>
+            </div>
+          </Panel>
+          <Panel
+            header={
+              <div className={style.header}>
+                <div>
+                  <p style={{ paddingLeft: 24 }}>Milho</p>
+                </div>
+                <div>
+                  <span>15</span> Espécies
+                </div>
+              </div>
+            }
+            key="2"
+          >
+            <div style={{ display: "flex" }}>
+              <p style={{ paddingLeft: 24 }}>Feijão</p>
+              <div>
+                <span>14</span> espécies
+              </div>
+            </div>
+          </Panel>
+          <Panel
+            header={
+              <div className={style.header}>
+                <div>
+                  <p style={{ paddingLeft: 24 }}>Fava</p>
+                </div>
+                <div>
+                  <span>15</span> Espécies
+                </div>
+              </div>
+            }
+            key="3"
+          >
+            <div style={{ display: "flex" }}>
+              <p style={{ paddingLeft: 24 }}>Feijão</p>
+              <div>
+                <span>14</span> espécies
+              </div>
+            </div>
+          </Panel>
+          <Panel
+            header={
+              <div className={style.header}>
+                <div>
+                  <p style={{ paddingLeft: 24 }}>Algodão</p>
+                </div>
+                <div>
+                  <span>15</span> Espécies
+                </div>
+              </div>
+            }
+            key="4"
+          >
+            <div style={{ display: "flex" }}>
+              <p style={{ paddingLeft: 24 }}>Hotaliças</p>
+              <div>
+                <span>14</span> espécies
+              </div>
+            </div>
+          </Panel>
+          <Panel
+            header={
+              <div className={style.header}>
+                <div>
+                  <p style={{ paddingLeft: 24 }}>Tomate</p>
+                </div>
+                <div>
+                  <span>15</span> Espécies
+                </div>
+              </div>
+            }
+            key="5"
+          >
+            <div style={{ display: "flex" }}>
+              <p style={{ paddingLeft: 24 }}>Feijão</p>
+              <div>
+                <span>14</span> espécies
+              </div>
+            </div>
+          </Panel>
+          <Panel
+            header={
+              <div className={style.header}>
+                <div>
+                  <p style={{ paddingLeft: 24 }}>Feijão</p>
+                </div>
+                <div>
+                  <span>15</span> Espécies
+                </div>
+              </div>
+            }
+            key="6"
+          >
+            <div style={{ display: "flex" }}>
+              <p style={{ paddingLeft: 24 }}>Feijão</p>
+              <div>
+                <span>14</span> espécies
+              </div>
+            </div>
+          </Panel>
+        </Collapse>
       </div>
     </Container>
   );
